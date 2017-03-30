31cac5c3c7bbcf4bbd4ae9b233b23db942a2b74a
David Bushell <david@dbushell.com>
2017-03-30 13:43:02 +0100

    nav refactor

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -4,33 +4,33 @@ import {Icon} from '../';
 
 const Nav = props => {
   return (
-    <nav className="nav nav--flex" id="nav">
-      <h2 className="nav__title">{props.heading}</h2>
-      <ul className="nav__list" data-root="true">
+    <nav className="b-nav" id="nav">
+      <h2 className="b-nav__title u-vh">{props.heading}</h2>
+      <ul className="b-nav__list" data-root="true">
         {props.items.map(item => (
-          <li key={item.order} className="nav__item" data-priority={item.priority} data-order={item.order}>
-            <a href={item.href} className="nav__link">{item.text}</a>
+          <li key={item.order} className="b-nav__item" data-priority={item.priority} data-order={item.order}>
+            <a href={item.href} className="b-nav__link">{item.text}</a>
           </li>
         ))}
-        <li className="nav__item nav__item--icons" data-priority={props.items.length + 1} data-order={props.items.length + 1}>
-          <a className="nav__link nav__link--twitter" rel="me noopener noreferrer" target="_blank" title="David Bushell on Twitter" href="http://twitter.com/dbushell">
+        <li className="b-nav__item b-nav__item--icons" data-priority={props.items.length + 1} data-order={props.items.length + 1}>
+          <a className="b-nav__link" rel="me noopener noreferrer" target="_blank" title="David Bushell on Twitter" href="http://twitter.com/dbushell">
             <Icon id="twitter"/>
-            <span>@dbushell</span>
+            <span className="u-vh">@dbushell</span>
           </a>
-          <a className="nav__link nav__link--github" rel="me noopener noreferrer" target="_blank" title="David Bushell on GitHub" href="https://github.com/dbushell/">
+          <a className="b-nav__link" rel="me noopener noreferrer" target="_blank" title="David Bushell on GitHub" href="https://github.com/dbushell/">
             <Icon id="github"/>
-            <span>GitHub</span>
+            <span className="u-vh">GitHub</span>
           </a>
-          <a className="nav__link nav__link--codepen" rel="me noopener noreferrer" target="_blank" title="David Bushell on CodePen" href="http://codepen.io/dbushell/">
+          <a className="b-nav__link" rel="me noopener noreferrer" target="_blank" title="David Bushell on CodePen" href="http://codepen.io/dbushell/">
             <Icon id="codepen"/>
-            <span>CodePen</span>
+            <span className="u-vh">CodePen</span>
           </a>
         </li>
-        <li className="nav__item nav__item--more">
-          <button type="button" className="nav__link">
+        <li className="b-nav__item b-nav__item--more">
+          <button type="button" className="b-nav__link">
             <Icon id="nav"/>
           </button>
-          <ul className="nav__list nav__dropdown"/>
+          <ul className="b-nav__list b-nav__dropdown"/>
         </li>
       </ul>
     </nav>
